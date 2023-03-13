593b06c12b51a6c03d74a9b81b482870388d9d30
vishalgupta888 <96484672+vishalgupta888@users.noreply.github.com>
2023-03-13 15:49:29 +0530

    structure created

---
+++ src/components/InteractSection/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const InteractSection = () => {
+  return (
+    <Box h='100vh' >
+        
+    </Box>
+  )
+}
+
+export default InteractSection
