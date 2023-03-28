c812366be7101ceec2f912063a23c23ba37eb61c
vishalgupta888 <96484672+vishalgupta888@users.noreply.github.com>
2023-03-28 14:37:42 +0530

    transition

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,38 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import overrideTheme from '@/styles/chakraStyles/theme'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { Box, ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 
 export default function App({ Component, pageProps }) {
   const theme = extendTheme(overrideTheme);
+
+  useEffect(() => {
+    document.addEventListener("scroll", function() {
+      var pageTop = document.scrollTop();
+      var pageBottom = pageTop + window.height();
+      var tags = (".tag");
+    
+      for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+    
+        if (tag.position().top < pageBottom) {
+          tag.addClass("visible");
+        } else {
+          tag.removeClass("visible");
+        }
+      }
+    });
+  }, [])
+  
+  
   return <ChakraProvider theme={theme}>
     <Navbar/>
+    <Box className='tag'>
     <Component {...pageProps} />
+    </Box>
     <Footer/>
   </ChakraProvider>
 }
